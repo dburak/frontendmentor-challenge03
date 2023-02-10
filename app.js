136cbde2bc85100a801d8ceffc3e4299b1bcd309
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-02-10 22:29:51 +0300

    additional feature disable button added

--- app.js
+++ app.js
@@ -8,12 +8,24 @@ let selectedRating = 0;
 
 ratings.forEach((rating) => {
   rating.addEventListener('click', (e) => {
+    btnSubmit.classList.remove('unchecked');
+
+    const active = document.querySelector('.selected');
+
+    console.log(active);
+
+    if (active) {
+      active.classList.remove('selected');
+    }
+
+    rating.classList.add('selected');
     selectedRating = e.target.innerText;
   });
 });
 
 btnSubmit.addEventListener('click', () => {
   ratingPoint.innerText = selectedRating;
+
   cardRating.classList.add('hidden');
   cardThankYou.classList.remove('hidden');
 });
